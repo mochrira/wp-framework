cc62cce3dd27a0f03caa186b69189085946fa708
Moch Rizal Rachmadani <mochrira@gmail.com>
2020-10-12 13:14:57 +0700

    adding helper

--- dist/Helper.php
+++ dist/Helper.php
@@ -4,3 +4,11 @@ function is_customizer() {
     global $wp_customize;
     return isset($wp_customize);
 }
+
+function get_template_part_content($slug, $name = null, $args = []) {
+    ob_start();
+    get_template_part($slug, $name, $args);
+    $content = ob_get_contents();
+    ob_end_clean();
+    return $content;
+}
