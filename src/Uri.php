f53962a237bee3e19ea490e93ebd88c4396da1c2
Moch. Rizal Rachmadani <mochrira@gmail.com>
2021-01-03 13:20:22 +0700

    rename pluginUri to pluginURl

--- src/Uri.php
+++ src/Uri.php
@@ -4,7 +4,7 @@ namespace WpFramework;
 
 class Uri {
 
-    public static function pluginUri($path = '') {
+    public static function pluginUrl($path = '') {
         return get_template_directory_uri().preg_replace('/'.preg_quote(get_template_directory(), '/').'/', '', $path, 1);
     }
 
